b870d5c6d1454f4ddbd179e463d7a4d7ce781855
James Tatum <jtatum@gmail.com>
2017-09-15 10:29:11 -0700

    Create target path first

--- recipes/venv.rb
+++ recipes/venv.rb
@@ -4,6 +4,11 @@ python_runtime node['slackbot']['python_ver'] do
   provider :system
 end
 
+directory node['slackbot']['target_dir'] do
+  owner node['slackbot']['user']
+  group node['slackbot']['group']
+end
+
 python_virtualenv env_path do
   user node['slackbot']['user']
   group node['slackbot']['group']
